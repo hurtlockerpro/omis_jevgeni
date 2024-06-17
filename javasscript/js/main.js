cdfa7c36e34d8c7309bc14089584c1cda1c9f882
vladimir kähri <info@hurtlocker.pro>
2024-06-17 20:35:08 +0300

    javascript vol 1

---
+++ javasscript/js/main.js
@@ -0,0 +1,23 @@
+
+let x = 10 // number
+let y  = "vladimir" // string
+let b = true // boolean  true / false
+let u = undefined 
+let o = [] // object -> array
+let o1 = {} // object -> array 
+
+// concatenation
+console.log("x=" + x);
+console.log(x + "x=");
+console.log(x + x); // 10 + 10
+
+
+let firstname = "Vladimir"
+
+console.log("Hello " + firstname);
+
+
+console.log(typeof x);
+console.log(typeof "15");
+
+
